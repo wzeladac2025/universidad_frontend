9db4e911da5b3b93d916b5ff1c3d12f9633faf79
Walter Zelada Castro <wzeladac@miumg.edu.gt>
2025-10-26 15:29:57 -0600

    (FEATURE) Notas

--- src/pages/IngresarNota.vue
+++ src/pages/IngresarNota.vue
@@ -1,54 +1,105 @@
-<script lang="ts" src="./controllers/Acceso"></script>
+<script lang="ts" src="./controllers/IngresarNota"></script>
 
 <template>
   <el-card style="margin: 25px;">
     <template #header>
       <div class="card-header">
-        <span>Registrar Acceso</span>
+        <span>Ingresar Notas</span>
       </div>
     </template>
 
     <!-- MAIN CONTENT -->
     <el-form ref="formRef" :model="form" :rules="rules" label-width="auto">
-      <el-form-item prop="rol" label="Rol">
-        <el-select v-model="form.rol" placeholder="Seleccione un rol">
-          <el-option v-for="item in perfiles" :key="item.value" :label="item.label" :value="item.value" />
+      <el-form-item prop="curso" label="Curso">
+        <el-select v-model="form.curso" placeholder="Seleccione un curso para cargar sus notas"
+          :change="cargarActividades" filterable>
         </el-select>
       </el-form-item>
-      <el-form-item prop="acceso" label="Acceso">
-        <el-input v-model="form.acceso" placeholder="Acceso" />
+      <el-form-item prop="actividad" label="Actividad">
+        <el-select v-model="form.actividad" placeholder="Seleccione una actividad para asignar notas"
+          :change="cargarListadoEstudiantesActividad" filterable>
+        </el-select>
       </el-form-item>
     </el-form>
 
-    <template #footer>
-      <el-button type="primary" @click="registrarUsuario()">Registrar
-        Acceso</el-button>
-    </template>
+    <!-- MAIN CONTENT -->
+    <el-card style="margin: 25px; margin-top: 50px;">
+      <template #header>
+        <div class="card-header">
+          <span>Actividad: @Actividad</span>
+        </div>
+      </template>
+
+      <!-- MAIN CONTENT -->
+      <el-descriptions class="margin-top" :column="1" title="Descripcion de Actividad" border>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">
+              Nombre de Actividad
+            </div>
+          </template>
+          Tarea #1
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">
+              Descripcion
+            </div>
+          </template>
+          Esta es la descripcion de la actividad
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">
+              Fecha de Entrega
+            </div>
+          </template>
+          2025-10-30
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">
+              Punteo Neto
+            </div>
+          </template>
+          5
+        </el-descriptions-item>
+      </el-descriptions>
+    </el-card>
   </el-card>
 
   <!-- LISTADO CURSOS -->
   <el-card style="margin: 25px;">
     <template #header>
       <div class="card-header">
-        <span>Listado de Accesos</span>
+        <span>Listado de estudiantes - Actividad: @Actividad</span>
       </div>
     </template>
 
     <!-- MAIN CONTENT -->
-    <el-table :data="listadoUsuarios" style="width: 100%;">
-      <el-table-column prop="rol.label" label="Rol" />
-      <el-table-column prop="acceso" label="Acceso" />
-      <el-table-column label="Acciones">
+    <el-form ref="formRefEstudiante" :model="formEstudiante" :rules="rulesEstudiante" label-width="auto">
+      <el-table :data="listadoEstudiantes" style="width: 100%;">
+        <el-table-column prop="nombre" label="Nombre" />
+        <el-table-column prop="descripcion" label="Descripcion" />
+        <el-table-column prop="fechaEntrega" label="Fecha de Entrega" />
+        <el-table-column prop="tipo.nombre" label="Tipo Actividad" />
+        <!-- <el-table-column label="Periodo">
         <template #default="scope">
-          <el-button size="small" @click="editar(scope.$index, scope.row)" :disabled="index != -1">
-            Editar
-          </el-button>
-          <el-button size="small" type="danger" @click="eliminar(scope.$index, scope.row)">
-            Eliminar
-          </el-button>
+          {{ periodo(scope.row.periodo) }}
         </template>
-      </el-table-column>
-    </el-table>
+      </el-table-column> -->
+        <el-table-column label="Ingresar Nota">
+          <template #default="scope">
+            <el-button size="small" @click="editar(scope.$index, scope.row)" :disabled="index != -1">
+              Editar
+            </el-button>
+            <el-button size="small" type="danger" @click="eliminar(scope.$index, scope.row)">
+              Eliminar
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-form>
   </el-card>
 </template>
 
--- src/pages/controllers/IngresarNota.ts
+++ src/pages/controllers/IngresarNota.ts
@@ -1,66 +1,115 @@
 import type { FormInstance, FormRules } from "element-plus";
 import { defineComponent, reactive, ref } from "vue";
+import { UploadFilled } from "@element-plus/icons-vue";
 
 export default defineComponent({
-  name: "Acceso",
+  name: "IngresarNota",
   setup() {
+    const formNode = reactive({
+      id: 0,
+      nota: 0
+    });
+
     return {
-      listadoUsuarios: [{
-        rol: {
-          label: "Estudiante",
-          value: "estudiante",
+      listadoEstudiantes: [
+        {
+          nombre: "Tarea #3",
+          descripcion: "Tarea Numero 3",
+          fechaCalificacion: new Date(),
+          fechaEntrega: new Date(),
+          tipo: {
+            id: 1,
+            nombre: "TAREA"
+          },
+          estado: {
+            id: 2,
+            nombre: "NO COMPLETADA",
+          },
+          nota: 0,
+          notaNeta: 0,
         },
-        acceso: "/general/home",
-      }],
+      ],
+      tiposActividad: [
+        {
+          id: 1,
+          nombre: "TAREA"
+        },
+        {
+          id: 2,
+          nombre: "ASISTENCIA"
+        },
+        {
+          id: 3,
+          nombre: "PRIMER PARCIAL"
+        },
+        {
+          id: 4,
+          nombre: "SEGUNDO PARCIAL"
+        },
+        {
+          id: 5,
+          nombre: "PROYECTO"
+        },
+        {
+          id: 6,
+          nombre: "EXAMEN FINAL"
+        },
+      ],
       index: -1,
       formRef: ref<FormInstance>(),
+      formRefEstudiante: ref<FormInstance>(),
       form: reactive({
-        rol: "",
-        acceso: ""
+        curso: null,
+        actividad: null,
       }),
+      formEstudiante: reactive({
+        nodo: formNode
+      }),   
       rules: reactive<FormRules>({
-        rol: [
+        curso: [
           {
             required: true,
-            message: "Seleccione un rol",
+            message: "El curso es requerido",
             trigger: "blur",
           },
         ],
-        acceso: [
+        actividad: [
           {
             required: true,
-            message: "La ruta de acceso es requerida",
+            message: "Debe seleccionar una actividad del curso",
             trigger: "blur",
-          }
+          },
         ],
       }),
-      perfiles: [
-        {
-          label: "Estudiante",
-          value: "estudiante",
-        },
-        {
-          label: "Docente",
-          value: "docente",
-        },
-        {
-          label: "Administrador",
-          value: "administrador",
-        },
-      ],
+      rulesEstudiante: reactive<FormRules>({
+        id: [
+          {
+            required: true,
+            message: "El curso es requerido",
+            trigger: "blur",
+          },
+        ],
+        nota: [
+          {
+            required: true,
+            message: "Debe seleccionar una actividad del curso",
+            trigger: "blur",
+          },
+        ],
+      }),      
     };
   },
   methods: {
-    async registrarUsuario() {
-      if (!this.formRef) return;
-      await this.formRef.validate((valid) => {
-        if (valid) {
+    cargarActividades() {
 
-        }
-      });
+    },
+    cargarListadoEstudiantesActividad () {
+
     },
     editar(indice: number, fila: any) { },
     eliminar(indice: number, fila: any) { },
   },
-  components: {},
+  components: {
+    UploadFilled,
+  },
 });
