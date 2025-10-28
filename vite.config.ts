7194f1371f93bb4d7c03e3d483aa97f2835a2170
Walter Zelada <wzelada@codigo-go.com>
2025-10-27 22:57:26 -0600

    (FEATURE) allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,9 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    allowedHosts: [
+      'https://universidad-frontend.onrender.com/'
+    ]
+  }
 });
