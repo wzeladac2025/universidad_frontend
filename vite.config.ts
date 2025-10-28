df73ecb69bf57a9e2aa5b57eec7c31365f84d96f
Walter Zelada <wzelada@codigo-go.com>
2025-10-28 13:21:57 -0600

    (FEATURE) Allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -13,7 +12,10 @@ export default defineConfig({
   },
   server: {
     allowedHosts: [
-      'universidad-frontend.onrender.com'
+      'universidad-frontend.onrender.com',
+      'localhost:8081',
+      '192.168.0.2:8081',
+      '25.5.74.72:8081'
     ]
   }
 });
